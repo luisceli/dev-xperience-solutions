a982c6dba8cfc19e8e5c0c65eee7a68c4b2fe524
luisceli <luisceli25@gmail.com>
2023-05-24 15:34:19 -0500

    Prueba rama frontendMarjan

--- LoginScreen.js
+++ LoginScreen.js
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginBottom: 16,
+    marginBottom: 16 ,
   },
   inputContainer: {
     marginBottom: 16,
