0852c9e6a826985d6c2accca87a0f5fffd02d133
luisceli <luisceli25@gmail.com>
2023-05-24 15:26:54 -0500

    primer commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import LoginScreen from './LoginScreen';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <LoginScreen />
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ LoginScreen.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Aquí puedes agregar la lógica para autenticar al usuario
+    // utilizando el correo electrónico (email) y la contraseña (password)
+    console.log('Iniciar sesión', email, password);
+  };
+
+  return (
+    <LinearGradient
+      colors={['#FFD700', '#0000FF']}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.gradientContainer}
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>Mi primera aplicación</Text>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Correo electrónico</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => setEmail(text)}
+            value={email}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Text style={styles.label}>Contraseña</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => setPassword(text)}
+            value={password}
+            secureTextEntry
+          />
+        </View>
+        <Button title="Iniciar sesión" onPress={handleLogin} />
+      </View>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  gradientContainer: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  inputContainer: {
+    marginBottom: 16,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+    backgroundColor:'#ffff',
+  },
+});
+
+export default LoginScreen;
